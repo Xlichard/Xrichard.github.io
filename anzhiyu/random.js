efb62d1f8a4aa0100feaa163251f6b32733bf090
xiongkuan <2336508035@qq.com>
2024-11-26 18:51:39 +0800

    Site updated: 2024-11-26 18:51:38

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2024/11/10/FirstTryBlog/","2024/11/26/Test/","2024/11/10/hello-world/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
